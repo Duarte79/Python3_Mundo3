fb1d0e85a540f504e778f94fd02a85404cf84e0d
Duarte Rodrigues <duarter56@gmail.com>
2024-04-11 20:49:07 -0300

    Update tuplas.py

--- tuplas.py
+++ tuplas.py
@@ -1,2 +1,10 @@
 lanche = ('Hamburguer', 'suco', 'Pizza', 'pudim')
-print(lanche[])
+print('A', lanche[2])
+print('B', lanche[1:3])
+print('C', lanche[2:])
+print('D', lanche[:2])
+print('E', lanche[-2:])
+print('F', lanche[3:])
+print(lanche)
+#tuplas são imutáveis
+
