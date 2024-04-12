eba7eac577b8481d463d523d4fa50e8c0089b004
Duarte Rodrigues <duarter56@gmail.com>
2024-04-12 20:27:55 -0300

    Aprendendo mais sobre tuplas

--- tuplas.py
+++ tuplas.py
@@ -1,10 +1,18 @@
-lanche = ('Hamburguer', 'suco', 'Pizza', 'pudim')
-print('A', lanche[2])
+'''lanche = ('Hamburguer', 'suco', 'Pizza', 'pudim')
+print('A', lanche[-2])
 print('B', lanche[1:3])
 print('C', lanche[2:])
 print('D', lanche[:2])
 print('E', lanche[-2:])
 print('F', lanche[3:])
-print(lanche)
+print(lanche)'''
 #tuplas são imutáveis
 
+'''lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Fritas')
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+print('Comi pra caramba!')'''
+
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Fritas')
+print(len(lanche))
+print('Comi pra caramba!')
