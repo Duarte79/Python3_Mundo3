b905d70bbe1c13c096a1a51e40710e7fa7b660f7
Duarte Rodrigues <duarter56@gmail.com>
2024-04-16 21:28:58 -0300

    Novos códigos incluídos

--- listas.py
+++ listas.py
@@ -6,43 +6,62 @@ print(num)'''
 num.append(7)
 print(num)'''
 
-num = [2, 5, 9, 1]
+'''num = [2, 5, 9, 1]
 num.sort()
-print(num)
+print(num)'''
 
-num = [2, 5, 9, 1]
+'''num = [2, 5, 9, 1]
 num.sort(reverse=True)
-print(num)
+print(num)'''
 
-num = [2, 5, 9, 1]
+'''num = [2, 5, 9, 1]
 num.sort(reverse=True)
-print(f'Essa lista tem {len(num)} elementos')
+print(f'Essa lista tem {len(num)} elementos')'''
 
-num = [2, 25, 9, 1, 3]
+'''num = [2, 25, 9, 1, 3]
 num.insert(2, 0)
-print(num)
+print(num)'''
 
-num = [2, 25, 9, 1, 3]
+'''num = [2, 25, 9, 1, 3]
 num.pop()
-print(num)
+print(num)'''
 
-num = [2, 25, 9, 1, 3]
+'''num = [2, 25, 9, 1, 3]
 num.pop(3)
-print(num)
+print(num)'''
 
-num = [2, 25, 9, 1, 3]
+'''num = [2, 25, 9, 1, 3]
 num.remove(25)
-print(num)
+print(num)'''
 
-num = [2, 25, 9, 1, 3]
+'''num = [2, 25, 9, 1, 3]
 if 25 in num:
     num.remove(25)
-    print(num)
+    print(num)'''
 
-valores = []
-valores.append(5)
-valores.append(9)
-valores.append(4)
-for v in valores:
-    print(f'{v}...', end='')
 
+'''valor = list()
+valor.append(5)
+valor.append(9)
+valor.append(4)
+for c, v in enumerate(valor):
+    print(f'na posição {c} encontrei o valor {valor}')'''
+
+'''valores = list()
+for cont in range(5):
+    valores.append(int(input('Digite um valor: ')))
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}')'''
+
+a = [1, 2, 3, 4, 5, 6]
+b = a
+b[2] = 8
+
+print('lisra A', a)
+print('lista B', b)
+
+a = [1, 2, 3, 4, 5, 6]
+b = a[:]
+b[2] = 8
+print('lista A', a)
+print('lista B', b)
